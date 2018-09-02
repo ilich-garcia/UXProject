d141c10601a4cd55feb8989c8c0f013505387436
yoshuadupin <djosue@united.edu>
2018-09-02 14:46:41 -0600

    Commit necesario para Ilich

--- uxproject/src/App.js
+++ uxproject/src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter , Switch , Route , Redirect } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/createClass" exact />
+          <Route path="/" exact />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
