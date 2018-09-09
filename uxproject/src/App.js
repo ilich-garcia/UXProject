83648ea73ceae6b18a0df0eaffb5834ad2624ca9
yoshuadupin <djosue@united.edu>
2018-09-09 08:34:50 -0600

    MenuAppBar borrado , Autentificacion incluida

--- uxproject/src/App.js
+++ uxproject/src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import MenuAppBar from "./components/MenuAppBar";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flexGrow: 1
+  }
+};
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {
-      user: null,
-      users: []
-    };
-
-    this.handleAuth = this.handleAuth.bind(this);
     this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleAuth = this.handleAuth.bind(this);
+  }
+  state = {
+    user: null
+  };
+
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        alert(`${result.user.email} ha iniciado sesión`);
+        this.setState({
+          user: firebase.auth().currentUser
+        });
+      })
+      .catch(error => {
+        alert(`Error ${error.code}: ${error.message}`);
+      });
+  }
+
+  handleLogOut() {
+    alert("¡Has cerrado sesión exitosamente!");
+    firebase.auth().signOut();
+    this.setState(state => ({
+      user: null
+    }));
   }
 
   componentWillMount() {
@@ -29,83 +67,63 @@ class App extends Component {
     });
   }
 
-  handleAuth() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(result => {
-        alert(`${result.user.email} ha iniciado sesión`);
-        this.setState(() => ({
-          alert: (
-            <div className="alert alert-success alert-dismissible fade show">
-              <button
-                onClick={this.handleAlert}
-                type="button"
-                className="close"
-                data-dismiss="alert"
-              >
-                &times;
-              </button>
-            </div>
-          )
-        }));
-      })
-      .catch(error => {
-        alert(`Error ${error.code}: ${error.message}`);
-        this.setState(() => ({
-          alert: (
-            <div className="alert alert-danger alert-dismissible fade show">
-              <button
-                onClick={this.handleAlert}
-                type="button"
-                className="close"
-                data-dismiss="alert"
-              >
-                &times;
-              </button>
-            </div>
-          )
-        }));
-      });
-  }
-
-  handleAlert() {
-    this.setState(() => ({
-      alert: null
-    }));
-  }
-
-  handleLogOut() {
-    this.setState({
-      alert: (
-        <div className="alert alert-success alert-dismissible fade show">
-          <button
-            onClick={this.handleAlert}
-            type="button"
-            className="close"
-            data-dismiss="alert"
-          >
-            &times;
-          </button>
-        </div>
-      )
-    });
-
-    alert("¡Has cerrado sesión exitosamente!");
-    firebase.auth().signOut();
-  }
-
   render() {
     return (
       <BrowserRouter>
         <div>
-          <MenuAppBar />
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="title" color="inherit" style={styles.flex}>
+                Grappther
+              </Typography>
+
+              <div>
+                <Link to="/perfil">
+                  <IconButton aria-haspopup="true" color="inherit">
+                    <Avatar
+                      src={this.state.user ? this.state.user.photoURL : ""}
+                    />
+                  </IconButton>
+                </Link>
+                <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>
+                  <Button aria-haspopup="true" color="inherit">
+                    HOME
+                  </Button>
+                </Link>
+                <Link
+                  to="/crearTutoria"
+                  style={{ color: "#fff", textDecoration: "none" }}
+                >
+                  <Button aria-haspopup="true" color="inherit">
+                    CREAR TUTORIA
+                  </Button>
+                </Link>
+                {this.state.user ? (
+                  <Button
+                    onClick={this.handleLogOut}
+                    aria-haspopup="true"
+                    color="inherit"
+                  >
+                    LOGOUT
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={this.handleAuth}
+                    aria-haspopup="true"
+                    color="inherit"
+                  >
+                    LOGIN
+                  </Button>
+                )}
+              </div>
+            </Toolbar>
+          </AppBar>
           <Switch>
-            <Route exact path="/">{<div>HOME</div>}</Route>
-            <Route path="/crearTutoria" >{<div>Crear Tutoria</div>}</Route> 
-            <Route path="/perfil" >{<div>PERFIL</div>}</Route>
+            <Route exact path="/">
+              {<div>HOME</div>}
+            </Route>
+            <Route path="/crearTutoria">{<div>Crear Tutoria</div>}</Route>
+            <Route path="/perfil">{<div>PERFIL</div>}</Route>
           </Switch>
         </div>
       </BrowserRouter>
--- uxproject/src/components/MenuAppBar.js
+++ uxproject/src/components/MenuAppBar.js
@@ -7,7 +7,9 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Avatar from "@material-ui/core/Avatar";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import firebase from "firebase";
+
 const styles = {
   root: {
     flexGrow: 1
@@ -18,6 +20,56 @@ const styles = {
 };
 
 class MenuAppBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: this.props.user
+    };
+  }
+
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        alert(`${result.user.email} ha iniciado sesión`);
+        this.setState({
+          user: firebase.auth().currentUser
+        });
+      })
+      .catch(error => {
+        alert(`Error ${error.code}: ${error.message}`);
+      });
+  }
+
+  handleAlert() {
+    this.setState(() => ({
+      alert: null
+    }));
+  }
+
+  handleLogOut() {
+    this.setState({
+      alert: (
+        <div className="alert alert-success alert-dismissible fade show">
+          <button
+            onClick={this.handleAlert}
+            type="button"
+            className="close"
+            data-dismiss="alert"
+          >
+            &times;
+          </button>
+        </div>
+      )
+    });
+
+    alert("¡Has cerrado sesión exitosamente!");
+    firebase.auth().signOut();
+  }
+
   render() {
     const { classes } = this.props;
 
@@ -31,21 +83,28 @@ class MenuAppBar extends React.Component {
           <div>
             <Link to="/perfil">
               <IconButton aria-haspopup="true" color="inherit">
-                <Avatar />
+                <Avatar >R</Avatar>
               </IconButton>
             </Link>
-            <Link to="/" style = {{color:"#fff" , textDecoration:"none"}}>
+            <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>
               <Button aria-haspopup="true" color="inherit">
                 HOME
               </Button>
             </Link>
-            <Link to="/crearTutoria" style = {{color:"#fff" , textDecoration:"none"}}>
+            <Link
+              to="/crearTutoria"
+              style={{ color: "#fff", textDecoration: "none" }}
+            >
               <Button aria-haspopup="true" color="inherit">
                 CREAR TUTORIA
               </Button>
             </Link>
-            <Button aria-haspopup="true" color="inherit">
-              LOGOUT
+            <Button
+              onClick={this.handleAuth}
+              aria-haspopup="true"
+              color="inherit"
+            >
+              LOGIN
             </Button>
           </div>
         </Toolbar>
