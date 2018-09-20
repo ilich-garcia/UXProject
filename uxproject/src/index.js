a9cb1e4641f8281ae2716dee27e43c8addfb4402
Andre Velasquez <avela504x@gmail.com>
2018-09-19 20:15:59 -0600

    modifying index

--- uxproject/src/index.js
+++ uxproject/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import * as auth from './auth/auth';
 import 'firebase/auth';
 import {firebase} from './firebase/firebase';
-//import * as firebase from './firebase/firebase';
 
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 export{
-    auth, 
     firebase,
 };
